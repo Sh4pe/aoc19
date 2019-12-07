6916104b06565e0eaa9d0c462abde317dfef3c2e
David Nies <david.nies@posteo.de>
2019-12-07 22:43:49 +0100

    Solve advent1, riddle 2

--- src/riddle/advent1.rs
+++ src/riddle/advent1.rs
@@ -24,9 +24,45 @@ impl Riddle for Advent1Riddle1 {
     }
 }
 
+pub struct Advent1Riddle2 {
+    input_file: String
+}
+
+impl Advent1Riddle2 {
+    pub fn new(input_file: &str) -> Advent1Riddle2 {
+        let input_file = input_file.to_string();
+        Advent1Riddle2{ input_file }
+    }
+}
+
+impl Riddle for Advent1Riddle2 {
+    fn solve(&self, _: &[String]) -> Result<Solution, RiddleError> {
+        let ints = ints_from_file(&self.input_file).unwrap();
+
+        let result = ints
+            .iter()
+            .map( |&x| calculate_fuel_recursive(x) )
+            .fold(0, |acc, x| acc + x );
+        Ok(Solution::Number(result))
+    }
+}
+
 pub fn calculate_fuel(mass: i64) -> i64 {
-    assert!(mass >= 6);
-    mass/3 - 2
+    assert!(mass >= 0);
+    if mass <= 6 {
+        0
+    } else {
+        mass/3 - 2
+    }
+}
+
+pub fn calculate_fuel_recursive(mass: i64) -> i64 {
+    let increment = calculate_fuel(mass);
+    if increment == 0 {
+        increment
+    } else {
+        increment + calculate_fuel_recursive(increment)
+    }
 }
 
 #[cfg(test)]
@@ -37,10 +73,25 @@ mod advent1_tests {
 
         #[test]
         fn it_works_as_expected() {
+            for i in 0..6 {
+                assert_eq!(calculate_fuel(i), 0);
+            }
             assert_eq!(calculate_fuel(12), 2);
             assert_eq!(calculate_fuel(14), 2);
             assert_eq!(calculate_fuel(1969), 654);
             assert_eq!(calculate_fuel(100756), 33583);
+        }
+    }
+
+    mod calculate_fuel_recursive_tests {
+        use super::super::calculate_fuel_recursive;
+
+        #[test]
+        fn it_works_as_expected() {
+            let test_cases = vec![(14, 2), (1969, 966), (100756, 50346)];
+            for (value, expected) in test_cases {
+                assert_eq!(calculate_fuel_recursive(value), expected);
+            }
         }
     }
 
@@ -56,4 +107,17 @@ mod advent1_tests {
             assert_eq!(solution, Solution::Number(3210097));
         }
     } 
+
+    mod riddle2_test {
+        use super::super::Advent1Riddle2;
+        use super::super::super::{Riddle, Solution};
+
+        #[test]
+        fn it_works_as_expected() {
+            let riddle = Advent1Riddle2::new("./data/input/1.txt");
+            let solution = riddle.solve(&vec![]).unwrap();
+
+            assert_eq!(solution, Solution::Number(4812287));
+        }
+    }
 }
--- src/riddle/riddlecontainer.rs
+++ src/riddle/riddlecontainer.rs
@@ -1,19 +1,22 @@
 use std::collections::{HashMap};
 
-use super::advent1::{Advent1Riddle1};
+use super::advent1::{Advent1Riddle1, Advent1Riddle2};
 use super::{Solution, RiddleError, Riddle};
 
 pub struct RiddleContainer {
     advent1_riddle1: Advent1Riddle1,
+    advent1_riddle2: Advent1Riddle2,
 }
 
 impl RiddleContainer {
     pub fn new() -> RiddleContainer {
 
         let advent1_riddle1 = Advent1Riddle1::new("./data/input/1.txt");
+        let advent1_riddle2 = Advent1Riddle2::new("./data/input/1.txt");
 
         RiddleContainer{
-            advent1_riddle1
+            advent1_riddle1,
+            advent1_riddle2
         }
     }
 
@@ -21,7 +24,8 @@ impl RiddleContainer {
         let riddle_map = {
             let mut riddle_map : HashMap<String, Box<dyn Fn(&[String]) -> Result<Solution, RiddleError>>> = HashMap::new();
 
-            riddle_map.insert("1_1".to_string(), Box::new(|args| self.advent1_riddle1.solve(args)));
+            riddle_map.insert("1.1".to_string(), Box::new(|args| self.advent1_riddle1.solve(args)));
+            riddle_map.insert("1.2".to_string(), Box::new(|args| self.advent1_riddle2.solve(args)));
 
             riddle_map
         };
