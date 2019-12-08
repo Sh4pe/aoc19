6c914c02336b1271651fb568a171fc3c5aafc610
David Nies <david.nies@posteo.de>
2019-12-08 22:08:06 +0100

    Implement `points_in_segment`

--- src/riddle/advent3.rs
+++ src/riddle/advent3.rs
@@ -2,12 +2,24 @@ use std::str;
 use std::num::{ParseIntError};
 use std::convert::{From};
 
+
 #[derive(Debug, Eq, PartialEq)]
-enum Segment {
+pub enum Segment {
     R(usize),
     L(usize),
     U(usize),
     D(usize),
+}
+
+impl Segment {
+    fn len(&self) -> usize {
+        match self {
+            Segment::U(n) => *n,
+            Segment::D(n) => *n,
+            Segment::L(n) => *n,
+            Segment::R(n) => *n,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -46,6 +58,35 @@ impl str::FromStr for Segment {
             'R' => Ok(Segment::R(length)),
             _ => Err(SegmentParseError::UnknownDirectoion(s))
         }
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Point {
+    pub x: i64,
+    pub y: i64
+}
+
+impl Point {
+    pub fn manhattan_norm(&self) -> usize {
+        (self.x.abs() as usize) + (self.y.abs() as usize)
+    }
+
+    pub fn points_in_seqment(&self, segment: Segment) -> Vec<Point> {
+        let (x_increment, y_increment) : (i64, i64) = match segment {
+            Segment::U(_) => (0,  1),
+            Segment::D(_) => (0, -1),
+            Segment::L(_) => (-1, 0),
+            Segment::R(_) => (1,  0),
+        };
+
+        let num_steps = segment.len() + 1;
+        (1..num_steps)
+            .map( |i| {
+                let i = i as i64;
+                Point{ x: self.x + i*x_increment, y: self.y + i*y_increment } 
+            })
+            .collect()
     }
 }
 
@@ -102,6 +143,37 @@ mod advent3_tests {
             }
         }
 
+    }
+
+    mod point_tests {
+        use super::super::{Point, Segment};
+
+        #[test]
+        fn the_norm_works_as_expected() {
+            let p = Point{ x: -3, y: 2 };
+            assert_eq!(p.manhattan_norm(), 5);
+        }
+
+        #[test]
+        fn points_in_seqment_works_as_expected() {
+            let test_cases : Vec<((i64, i64), _, Vec<(i64, i64)>)> = vec![
+                ((3,2),  Segment::R(4), vec![(4,2), (5,2), (6,2), (7,2)]),
+                ((1,-1), Segment::U(2), vec![(1,0), (1,1)]),
+                ((0, 0), Segment::L(3), vec![(-1,0), (-2,0), (-3,0)]),
+                ((4, 4), Segment::D(4), vec![(4,3), (4,2), (4,1), (4,0)]),
+            ];
+
+            for (origin, segment, expected) in test_cases {
+                let origin = Point{ x: origin.0, y: origin.1 };
+                let expected : Vec<Point> = expected
+                    .iter()
+                    .map( |p| Point{ x: p.0, y: p.1} )
+                    .collect();
+                let points_in_segment = origin.points_in_seqment(segment);
+
+                assert_eq!(points_in_segment, expected);
+            }
+        }
     }
 
 }
